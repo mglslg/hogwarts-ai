ef30ca4caf94052c19ed914a87a3dafe341dbf2f
suolongga <mglslg@qq.com>
2023-11-19 20:07:21 +0800

    test

--- src/openai_api/chat.py
+++ src/openai_api/chat.py
@@ -12,3 +12,19 @@ def send_msg(msg):
         ]
     )
     return completion.choices[0].message
+
+def send_msg_stream(msg):
+    responses = []
+    with OpenAI.ChatCompletion.stream(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": msg}
+        ]
+    ) as stream:
+        for response in stream:
+            responses.append(response)
+            # You can process each part of the response as it arrives
+            # For example, print it, store it, etc.
+            print(response)
+    return responses
