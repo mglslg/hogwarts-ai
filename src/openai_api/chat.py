933cb43eb85866900de66f78c41c5cb57a58f11a
01420524 <suolongga@sf-express.com>
2023-11-20 18:51:26 +0800

    fun

--- src/openai_api/chat.py
+++ src/openai_api/chat.py
@@ -1,3 +1,5 @@
+import time
+
 from openai import OpenAI
 
 client = OpenAI()
@@ -13,18 +15,44 @@ def send_msg(msg):
     )
     return completion.choices[0].message
 
+
 def send_msg_stream(msg):
-    responses = []
-    with OpenAI.ChatCompletion.stream(
+    # record the time before the request is sent
+    start_time = time.time()
+
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": msg}
-        ]
-    ) as stream:
-        for response in stream:
-            responses.append(response)
-            # You can process each part of the response as it arrives
-            # For example, print it, store it, etc.
-            print(response)
-    return responses
+        ],
+        stream=True
+    )
+
+    # create variables to collect the stream of chunks
+    collected_chunks = []
+    collected_messages = []
+
+    # iterate through the stream of events
+    for chunk in response:
+        chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+        collected_chunks.append(chunk)  # save the event response
+
+        # Check if there are choices and extract the message from the first choice
+        if chunk.choices:
+            first_choice = chunk.choices[0]  # get the first choice
+            delta = first_choice.delta  # get the delta from the choice
+            chunk_message = delta.content if delta.content is not None else ''  # get the content from the delta
+            collected_messages.append(chunk_message)  # save the message
+            print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")  # print the delay and text
+        else:
+            print(f"No choices available for this chunk, received after {chunk_time:.2f} seconds")
+
+    # print the time delay and text received
+    print(f"Full response received {chunk_time:.2f} seconds after request")
+    full_reply_content = ''.join(collected_messages)  # join all collected messages
+    print(f"Full conversation received: {full_reply_content}")
+
+
+send_msg_stream('chunk是什么意思')
+
