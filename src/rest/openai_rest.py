a7a1b7da176e0f01f4b9723c94e8088c01aff114
01420524 <suolongga@sf-express.com>
2023-11-21 18:34:08 +0800

    fun

--- src/rest/openai_rest.py
+++ src/rest/openai_rest.py
@@ -3,8 +3,17 @@ from openai_api import chat
 
 
 def init_app(curr_app):
-    @curr_app.route('/chat/single', methods=['POST'])
-    def index():
+    @curr_app.route('/chat/once', methods=['POST'])
+    def chat_once():
+        if not request.is_json:
+            return jsonify({'error': 'Invalid content type'}), 400
+        data = request.get_json()
+        content = chat.send_msg(data['msg']).content
+        return jsonify({'replay': content}), 200
+
+
+    @curr_app.route('/chat/context', methods=['POST'])
+    def chat_context():
         if not request.is_json:
             return jsonify({'error': 'Invalid content type'}), 400
         data = request.get_json()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,6 @@
-import unittest
-from openai_api import assistant
-from openai_api import chat
-
-
 def test1():
     print('__name__:', __name__)
     print('__file__:', __file__)
     print('__doc__:', __doc__)
     print('__package__:', __package__)
     print('__builtins__:', __builtins__)
-
-
-test1()
