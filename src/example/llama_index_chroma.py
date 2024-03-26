ba63a19ecdea817471ea85323bf7bfc28b6c7749
01420524 <suolongga@sf-express.com>
2024-03-26 19:09:50 +0800

    import chroma

--- src/example/llama_index_chroma.py
+++ src/example/llama_index_chroma.py
@@ -1,16 +1,24 @@
 import chromadb
-from llama_index.vector_stores import ChromaVectorStore
-from llama_index.storage.storage_context import StorageContext
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index_client.types import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.core.readers.file.base import SimpleDirectoryReader
 
 
 def llama_index_chroma():
+    # load some documents
+    # documents = SimpleDirectoryReader(input_dir="./data", input_files=[".data/questions.txt"]).load_data()
+    documents = SimpleDirectoryReader(input_dir="./data").load_data()
+
+    # initialize client, setting path to save data
+    db = chromadb.PersistentClient(path="./chroma_db_llama_index")
+
+    # create collection
+    chroma_collection = db.get_or_create_collection("quickstart")
+
     # assign chroma as the vector_store to the context
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    documents = SimpleDirectoryReader("./data").load_data()
-
-    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
     # create your index
     index = VectorStoreIndex.from_documents(
@@ -19,37 +27,9 @@ def llama_index_chroma():
 
     # create a query engine and query
     query_engine = index.as_query_engine()
-    response = query_engine.query("What is the meaning of life?")
+    response = query_engine.query("伏地魔是谁？")
     print(response)
 
-    db = chromadb.PersistentClient(path="./chroma_db")
-    chroma_collection = db.get_or_create_collection("quickstart")
 
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-    index = VectorStoreIndex.from_vector_store(vector_store, storage_context=storage_context)
-
-    query_engine = index.as_query_engine()
-    response = query_engine.query("What is llama2?")
-    print(response)
-
-    # initialize client
-    db = chromadb.PersistentClient(path="./chroma_db")
-
-    # get collection
-    chroma_collection = db.get_or_create_collection("quickstart")
-
-    # assign chroma as the vector_store to the context
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-    # load your index from stored vectors
-    index = VectorStoreIndex.from_vector_store(
-        vector_store, storage_context=storage_context
-    )
-
-    # create a query engine
-    query_engine = index.as_query_engine()
-    response = query_engine.query("What is llama2?")
-    print(response)
+if __name__ == '__main__':
+    llama_index_chroma()
